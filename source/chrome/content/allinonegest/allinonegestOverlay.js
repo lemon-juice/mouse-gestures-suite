975409af518a519278f5ae3e53cbf837dd9cec67
Michal <michal.lemon@gmail.com>
2014-02-17 01:41:15 +0100

    fix: right-click context menu pop ups and interferes with gesture on Linux when "Allow scripts to... Disable or replace context menu" is unchecked in browser preferences

--- source/chrome/content/allinonegest/allinonegestOverlay.js
+++ source/chrome/content/allinonegest/allinonegestOverlay.js
@@ -32,6 +32,7 @@ var aioOnImage = null; // contains an image DOM node
 var aioSrcEvent; // event which started the active gesture
 var aioBundle; // String bundle for localized strings
 var aioShowContextMenu = false;
+var aioAllowPopupShowing = true;
 var aioGestEnabled, aioRockEnabled;  // prefs ....
 var aioTrailEnabled, aioTrailColor, aioTrailSize, aioTrailOpacity;
 var aioWheelEnabled, aioScrollEnabled, aioNoScrollMarker, aioStartOnLinks;
@@ -414,8 +415,9 @@ function aioInit() { // overlay has finished loading or a pref was changed
      aioMainWin = document.getElementById("main-window");
      aioStatusBar = document.getElementById("statusbar-display");
     
-     aioRendering.addEventListener("contextmenu", aioContextMenuEnabler, true);
-     aioRendering.addEventListener("mousedown", aioMouseDown, true);
+     window.addEventListener("mousedown", aioMouseDown, true);
+     document.documentElement.addEventListener("popupshowing", aioPopupShowing, true);
+     document.documentElement.addEventListener("contextmenu", aioContextMenuEnabler, true);
 
      var activeId = "" + aioUnique++;
      aioContent.mTabContainer.childNodes[0].setAttribute('aioTabId', activeId);
@@ -485,6 +487,12 @@ function aioIsUnformattedXML(aDoc) {
 function aioContextMenuEnabler(e) {
   if (aioShowContextMenu) return;
   e.preventDefault(); e.stopPropagation();
+}
+
+function aioPopupShowing(e) {
+  if (!aioAllowPopupShowing && !aioShowContextMenu && e.originalTarget.nodeName == "menupopup") {
+    e.preventDefault(); e.stopPropagation();
+  }
 }
 
 function aioKeyPressed(e) {
@@ -657,7 +665,10 @@ function aioMouseDown(e) {
               aioStrokes = []; aioLocaleGest = []; aioCurrGest = "";
               if (aioTrailEnabled) aioStartTrail(e);
               window.addEventListener("mousemove", aioGestMove, true);
-           }
+              if (e.button == aioRMB) {
+                aioAllowPopupShowing = false;
+              }
+         }
            else preventDefaultAction = e.button != aioLMB;
         }
         // it can be the start of a wheelscroll gesture as well
@@ -762,6 +773,9 @@ function aioMouseUp(e) {
         if (aioRockTimer) clearTimeout(aioRockTimer);
         aioRockTimer = null;
      }
+  }
+  if (button == aioRMB) {
+    aioAllowPopupShowing = true;
   }
   if (aioGestInProgress) {
      var lastgesture = aioStrokes.join("");
