db048cd2777adaf6f5d88cf7ace1eeea2fb02c2f
Michal <michal.lemon@gmail.com>
2014-02-19 12:17:54 +0100

    - rename instances of "All-in-One Gestures" to "Mouse Gestures Suite" and the like  - add home page URL  - bump version number

--- aio.bat
+++ aio.bat
@@ -5,7 +5,7 @@ xcopy "D:\dev\aio-gestures\source" "D:\dev\aio-gestures-build" /I /E /Q
 
 for /D %%f in ("D:\dev\aio-gestures-build\chrome\*") do rmdir %%f /S /Q
 
-del "D:\dev\aio-gestures\mouse-gestures-suite-1.0.1.xpi" 2>nul
-"C:\Program Files\7-Zip\7z.exe" a -tzip "D:\dev\aio-gestures\mouse-gestures-suite-1.0.1.xpi" "D:\dev\aio-gestures-build\*"
+del "D:\dev\aio-gestures\mouse-gestures-suite-1.0.2.xpi" 2>nul
+"C:\Program Files\7-Zip\7z.exe" a -tzip "D:\dev\aio-gestures\mouse-gestures-suite-1.0.2.xpi" "D:\dev\aio-gestures-build\*"
 
 rmdir "D:\dev\aio-gestures-build" /S /Q
