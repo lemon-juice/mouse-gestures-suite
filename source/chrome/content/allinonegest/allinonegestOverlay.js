46519d25802588dfe7db59c54bc5dd3eb6f28e73
Michal <michal.lemon@gmail.com>
2014-03-19 01:32:22 +0100

    better detection of unformatted xml (so that rdf+xml is also detected)

--- source/chrome/content/allinonegest/allinonegestOverlay.js
+++ source/chrome/content/allinonegest/allinonegestOverlay.js
@@ -517,7 +517,7 @@ function aioIsKeyOK(e) {
 }
 
 function aioIsUnformattedXML(aDoc) {
-  return aDoc.contentType.indexOf("/xml") != -1 && aDoc.styleSheets && aDoc.styleSheets.length && 
+  return /\/[\w+]*xml/.test(aDoc.contentType) && aDoc.styleSheets && aDoc.styleSheets.length && 
          aDoc.styleSheets[0].href.substr(-31) == "/content/xml/XMLPrettyPrint.css";
 }
 
