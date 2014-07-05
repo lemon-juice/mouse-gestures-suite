9ac868878c5ad137b9a8ddcf9b762e817683b9ca
Michal <michal.lemon@gmail.com>
2014-07-05 22:04:53 +0200

    fix for semi-transparent trails hiding behind window-mode flash

--- source/chrome/content/allinonegest/trails.js
+++ source/chrome/content/allinonegest/trails.js
@@ -1,8 +1,8 @@
 /*
  * Drawing gesture trails
  */
-var aioTrailCont = null;
-var aioCtx, aioTrailPoints;;
+var aioTrailCont, aioTimeoutTrailCont;
+var aioCtx, aioTimeoutCtx, aioTrailPoints;;
 var aioDocX, aioDocY;
 var minX, miny, maxX, maxY;
 var haftTrailSize;
@@ -51,16 +51,26 @@ function aioDrawTrail(e) {
   if (!insertionNode) return;
   
   if (!aioTrailCont) {
-	aioMakeTrailCanvas();
+	var cnv = aioMakeTrailCanvas();
+	aioTrailCont = cnv.canvas;
+	aioCtx = cnv.ctx;
+	
+	if (!aioSmoothTrail) {
+	  aioSetCtxProperties(aioCtx, 1);
+	}
   }
-  aioTrailCont.style.opacity = 1;
+  
+  if (aioTimeoutTrailCont && aioTrailCont.style.display == 'none') {
+	aioTimeoutTrailCont.style.display = 'none';
+	aioTrailCont.style.display = 'block';
+  }
   
   var x = e.screenX - aioDocX;
   var y = e.screenY - aioDocY;
+  aioTrailPoints.push([x, y]);
   
   if (aioSmoothTrail) {
 	// erasing all canvas and drawing all line again results in smooth line
-	aioTrailPoints.push([x, y]);
 	
 	// we make canvas the size only as large as necessary, because full size canvas
 	// cause flash objects in window mode to disappear
@@ -78,10 +88,10 @@ function aioDrawTrail(e) {
 	
 	aioTrailCont.style.left = minX + "px";
 	aioTrailCont.style.top = minY + "px";
-	aioTrailCont.width = maxX - minX;
+	aioTrailCont.width = maxX - minX; // this erases canvas
 	aioTrailCont.height = maxY - minY;
 	
-	aioSetCtxProperties();
+	aioSetCtxProperties(aioCtx, 1);
 	  
 	var shiftX = aioTrailCont.offsetLeft;
 	var shiftY = aioTrailCont.offsetTop;
@@ -101,49 +111,92 @@ function aioDrawTrail(e) {
 }
 
 function aioMakeTrailCanvas() {
-  aioTrailCont = document.createElementNS(xhtmlNS, "canvas");
-  aioTrailCont.style.position = "fixed";
-  aioTrailCont.width = window.outerWidth;
-  aioTrailCont.height = window.outerHeight;
-  aioTrailCont.style.top = "0";
-  aioTrailCont.style.left = "0";
-  aioTrailCont.style.pointerEvents = "none";
-  aioTrailCont.style.zIndex = 10000;
+  var canvas = document.createElementNS(xhtmlNS, "canvas");
+  canvas.style.position = "fixed";
+  canvas.width = window.outerWidth;
+  canvas.height = window.outerHeight;
+  canvas.style.top = "0";
+  canvas.style.left = "0";
+  canvas.style.pointerEvents = "none";
+  canvas.style.zIndex = 10000;
   
-  insertionNode.appendChild(aioTrailCont);
+  insertionNode.appendChild(canvas);
 
-  aioCtx = aioTrailCont.getContext('2d');
-  aioSetCtxProperties();
+  var ctx = canvas.getContext('2d');
   
   if (!aioSmoothTrail) {
-	aioCtx.beginPath();
-	aioCtx.moveTo(aioTrailPoints[0][0], aioTrailPoints[0][1]);
+	ctx.beginPath();
+	ctx.moveTo(aioTrailPoints[0][0], aioTrailPoints[0][1]);
   }
+  
+  return {
+	canvas: canvas,
+	ctx: ctx
+  };
 }
 
-function aioSetCtxProperties() {
-  aioCtx.lineWidth = aioTrailSize;
-  aioCtx.lineJoin = 'round';
-  aioCtx.lineCap = 'round';
-  aioCtx.strokeStyle = aioTrailColor;
+function aioSetCtxProperties(ctx, opacity) {
+  ctx.lineWidth = aioTrailSize;
+  ctx.lineJoin = 'round';
+  ctx.lineCap = 'round';
+  ctx.strokeStyle = aioTrailColor;
+  ctx.globalAlpha = opacity;
 }
 
 function aioIndicateGestureTimeout() {
- try {
-   if (aioTrailCont) {
-	  aioTrailCont.style.opacity = 0.5;
-   }
-   aioStatusBar.label += "  [X]";
+  try {
+    if (aioTrailCont) {
+	  aioTrailCont.style.display = 'none';
+	  
+	  if (!aioTimeoutTrailCont) {
+		// instead of simply changing opacity of main canvas, we create another canvas
+		// where we draw the same trail with smaller opacity line - we do this because
+		// changing opacity of canvas causes it to hide behind window-mode flash objects
+		var cnv = aioMakeTrailCanvas();
+		aioTimeoutTrailCont = cnv.canvas;
+		aioTimeoutCtx = cnv.ctx;
+	  } else {
+		// reuse canvas created earlier
+		aioTimeoutTrailCont.style.display = 'block';
+	  }
+	  aioTimeoutTrailCont.style.top = aioTrailCont.style.top;
+	  aioTimeoutTrailCont.style.left = aioTrailCont.style.left;
+	  aioTimeoutTrailCont.width = aioTrailCont.width;
+	  aioTimeoutTrailCont.height = aioTrailCont.height;
+	  
+	  // draw all trail again with smaller opacity
+	  aioSetCtxProperties(aioTimeoutCtx, 0.5);
+	  
+	  var shiftX = aioTimeoutTrailCont.offsetLeft;
+	  var shiftY = aioTimeoutTrailCont.offsetTop;
+	  
+	  aioTimeoutCtx.beginPath();
+	  aioTimeoutCtx.moveTo(aioTrailPoints[0][0] - shiftX, aioTrailPoints[0][1] - shiftY);
+	  
+	  for (var i = 1, len=aioTrailPoints.length; i < len; i++) {
+		aioTimeoutCtx.lineTo(aioTrailPoints[i][0] - shiftX, aioTrailPoints[i][1] - shiftY);
+	  }
+	  aioTimeoutCtx.stroke();
+    }
+    aioStatusBar.label += "  [X]";
   
   } catch(err) {}
 }
 
 function aioEraseTrail() {
   aioTrailPoints = null;
+  // try-catch to prevent errors on page transitions during gesture drawing
   try {
-     aioTrailCont.parentNode.removeChild(aioTrailCont);
+	if (aioTrailCont) {
+	  aioTrailCont.parentNode.removeChild(aioTrailCont);
+	}
+    
+	if (aioTimeoutTrailCont) {
+	  aioTimeoutTrailCont.parentNode.removeChild(aioTimeoutTrailCont);
+	}
   }
   catch(err) {}
   aioTrailCont = null;
+  aioTimeoutTrailCont = null;
   insertionNode = null;
 }
