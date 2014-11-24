bb0ee7f264ae45f2db621cd35c86d138635fac7a
Michal <michal.lemon@gmail.com>
2014-11-24 12:52:07 +0100

    refactor: move aioDetectUrl() to utils

--- source/chrome/content/allinonegest/gestimp.js
+++ source/chrome/content/allinonegest/gestimp.js
@@ -690,7 +690,7 @@ function aioVScrollDocument(relativeScroll, aValue) {
 function aioSelectionAsURL(reverseBg) {
   var url = mgsuiteUtil.getSelectedText();
   
-  var urlToLoad = aioDetectUrl(url);
+  var urlToLoad = mgsuiteUtil.detectUrl(url);
   
   if (!urlToLoad) {
     // invalid address, do web search instead
@@ -2251,78 +2251,6 @@ function aioOpenConsole() {
   } else {
     toJavaScriptConsole();
   }
-}
-
-/* Check if string is a URL - return URL if valid */
-function aioDetectUrl(input) {
-  input = input.replace(/^[\s\'\"\(\[<]+/, ""); // strip bad leading characters
-  input = input.replace(/[\.,\'\"\)\?!>\]]+$/, ""); // strip bad ending characters
-  input = input.trim();
-  
-  if (input.indexOf("about:") == 0) {
-    return input;
-  }
-  
-  if (!input || /\s/.test(input)) {
-    return false;
-  }
-  
-  if (/^(http|https|ftp):\/\/\w/i.test(input)) {
-    // URL with scheme
-    return input;
-  }
-  
-  var host;
-  
-  if (/\//.test(input)) {
-    var segm = input.split('/');
-    host = segm[0].toLowerCase();
-  } else {
-    host = input.toLowerCase();
-  }
-  // match segments in host
-  var matches = /^([^@]+@)?((?:[\w-]+\.)*)([\w-]+)(:\d{1,5})?$/.exec(host);
-  
-  if (!matches || !matches[3]) {
-    return false;
-  }
-  
-  if (matches[1] && !matches[4] && !/\//.test(input)) {
-    // probably email address
-    return false;
-  }
-  
-  var domain = (typeof matches[2] != 'undefined') ? matches[2] : "";
-  domain += (typeof matches[3] != 'undefined') ? matches[3] : "";
-  
-  if (/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})+$/.test(domain)
-      || domain == 'localhost'
-      || /\.localhost$/.test(domain)) {
-    // IP or localhost
-    return input;
-  }
-  
-  if (!matches[2]) {
-    return false;
-  }
-  
-  // check top level domain
-  var tld = matches[3];
-  
-  // load list of top level domains
-  // see http://data.iana.org/TLD/tlds-alpha-by-domain.txt
-  var str = "";
-  var req = new XMLHttpRequest();
-  try {
-    req.open("GET", mgsuite.CHROME_DIR + "tlds.txt", false);
-    req.send();
-    str = req.responseText.trim();
-    
-  } catch (err){};
-  
-  var tlds = str.toLowerCase().split(/\s+/);
-  
-  return (tlds.indexOf(tld) >= 0) ? input : false;
 }
 
 function aioNullAction() {
