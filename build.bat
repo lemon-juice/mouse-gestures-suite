04cde69dd5f0c3c182b320a15bcb124eae68d752
Michal <michal.lemon@gmail.com>
2015-02-18 11:24:51 +0100

    version bump

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-set xpi_name=mouse-gestures-suite-1.7.0beta10.xpi
+set xpi_name=mouse-gestures-suite-1.7.0beta11.xpi
 
 rmdir "D:\dev\aio-gestures-build" /S /Q
 xcopy "D:\dev\aio-gestures\source" "D:\dev\aio-gestures-build" /I /E /Q
