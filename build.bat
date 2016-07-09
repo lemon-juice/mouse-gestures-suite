9361992ce9d1330b811beb990cbaa7baa67f0522
Michal <michal.lemon@gmail.com>
2016-07-09 15:23:55 +0200

    - do not pack files in jar (suggested by AMO reviewer)  - version 2.4.0

--- build.bat
+++ build.bat
@@ -1,15 +1,6 @@
-set xpi_name=mouse-gestures-suite-2.3.2.xpi
-
-rmdir "D:\dev\aio-gestures-build" /S /Q
-xcopy "D:\dev\aio-gestures\source" "D:\dev\aio-gestures-build" /I /E /Q
-
-"C:\Program Files\7-Zip\7z.exe" a -tzip -mx0 "D:\dev\aio-gestures-build\chrome\allinonegest.jar" "D:\dev\aio-gestures-build\chrome\*"
-
-for /D %%f in ("D:\dev\aio-gestures-build\chrome\*") do rmdir %%f /S /Q
+set xpi_name=mouse-gestures-suite-2.4.0.xpi
 
 del "D:\dev\aio-gestures\%xpi_name%" 2>nul
-"C:\Program Files\7-Zip\7z.exe" a -tzip "D:\dev\aio-gestures\%xpi_name%" "D:\dev\aio-gestures-build\*"
-
-rmdir "D:\dev\aio-gestures-build" /S /Q
+"C:\Program Files\7-Zip\7z.exe" a -tzip "D:\dev\aio-gestures\%xpi_name%" "D:\dev\aio-gestures\source\*"
 
 copy /B "D:\dev\aio-gestures\%xpi_name%" "D:\www\test\xpi\%xpi_name%"
